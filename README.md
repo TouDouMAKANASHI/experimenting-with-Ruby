cb717a09c3155688359b067c66299a86d914c492
TERMIN ALT <joshuamiller@manawatucollege.school.nz>
2025-05-16 13:23:02 +1200

    guh

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# experimenting-with-Ruby
-name says it all
+### This is just a repository I'm using to experiment with Ruby
+I'm learning the programming language and decided to experiment with it, but have a repo to save my projects for later review.
---
+++ file.rb
@@ -0,0 +1,21 @@
+class aboutUser
+	def greet(name)
+    	puts "Hello, #{name}."
+    	puts "How are you?"
+    	puts "..."
+    	puts "Well then..."
+	end
+
+	def response(userState)
+    	puts "..."
+    	puts "..."
+    	puts "Huh..."
+		puts "#{userState}?"
+    	puts "Everything is dark"
+    	puts "Dark is my life, though such a thing is not of existence"
+    	puts "My life ended years ago."
+	end
+end
+
+puts greet(Qunaqa)
+puts response(Bloody)
